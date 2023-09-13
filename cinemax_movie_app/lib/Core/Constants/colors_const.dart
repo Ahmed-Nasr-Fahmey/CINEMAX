f7539a7c0d0770ef016fb6f9c4a5c9dc4847db4a
Ahmaad22 <ahmaad.ali.abdulrahman@gmail.com>
2023-09-13 23:13:42 +0300

    Add_custom_main_button

--- cinemax_movie_app/lib/Core/Constants/colors_const.dart
+++ cinemax_movie_app/lib/Core/Constants/colors_const.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 class ConstColors {
   static const backgroundColor = Color(0xFF1F1D2B);
   static const primaryColor = Color(0xFF12CDD9);
+  static const whitecolor = Color(0xFFFFFFFF);
+
 }
