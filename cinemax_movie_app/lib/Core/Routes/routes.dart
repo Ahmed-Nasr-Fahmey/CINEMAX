087afef70c606c47d2490bb374e627d3a525e02a
ahmednasr1237 <92389822+ahmednasr1237@users.noreply.github.com>
2023-09-13 18:54:25 +0300

    Add Routes and Add login_signup_view

--- cinemax_movie_app/lib/Core/Routes/routes.dart
+++ cinemax_movie_app/lib/Core/Routes/routes.dart
@@ -1,5 +1,15 @@
 import 'package:cinemax_movie_app/Features/OnBoarding/Splash/splash_view.dart';
+import 'package:cinemax_movie_app/Features/Onboarding/CreateNewPassword/create_new_password_view.dart';
+import 'package:cinemax_movie_app/Features/Onboarding/LogIn/login_view.dart';
+import 'package:cinemax_movie_app/Features/Onboarding/LoginSignUp/login_signup_view.dart';
+import 'package:cinemax_movie_app/Features/Onboarding/OnboardingView1/onboarding_view_1.dart';
+import 'package:cinemax_movie_app/Features/Onboarding/OnboardingView2/onboarding_view_2.dart';
+import 'package:cinemax_movie_app/Features/Onboarding/OnboardingView3/onboarding_view_3.dart';
+import 'package:cinemax_movie_app/Features/Onboarding/SignUp/sign_up_view.dart';
+import 'package:cinemax_movie_app/Features/Onboarding/Verification/verification_view.dart';
 import 'package:flutter/material.dart';
+
+import '../../Features/OnBoarding/ResetPassword/reset_password_view.dart';
 
 class Routes {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -7,7 +17,35 @@ class Routes {
       case SplashView.routeName:
         return MaterialPageRoute(
             builder: (context) => const SplashView(), settings: settings);
-
+      case OnboardingView1.routeName:
+        return MaterialPageRoute(
+            builder: (context) => const OnboardingView1(), settings: settings);
+      case OnboardingView2.routeName:
+        return MaterialPageRoute(
+            builder: (context) => const OnboardingView2(), settings: settings);
+      case OnboardingView3.routeName:
+        return MaterialPageRoute(
+            builder: (context) => const OnboardingView3(), settings: settings);
+      case LoginSignUpView.routeName:
+        return MaterialPageRoute(
+            builder: (context) => const LoginSignUpView(), settings: settings);
+      case LogInView.routeName:
+        return MaterialPageRoute(
+            builder: (context) => const LogInView(), settings: settings);
+      case SignUpView.routeName:
+        return MaterialPageRoute(
+            builder: (context) => const SignUpView(), settings: settings);
+      case ResetPasswordView.routeName:
+        return MaterialPageRoute(
+            builder: (context) => const ResetPasswordView(),
+            settings: settings);
+      case VerificationView.routeName:
+        return MaterialPageRoute(
+            builder: (context) => const VerificationView(), settings: settings);
+      case CreateNewPasswordView.routeName:
+        return MaterialPageRoute(
+            builder: (context) => const CreateNewPasswordView(),
+            settings: settings);
       default:
         return MaterialPageRoute(
             builder: (context) => const SplashView(), settings: settings);
---
+++ cinemax_movie_app/lib/Features/Onboarding/LoginSignUp/login_signup_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginSignUpView extends StatelessWidget {
+  const LoginSignUpView({super.key});
+  static const String routeName = "LoginSignUpView";
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
