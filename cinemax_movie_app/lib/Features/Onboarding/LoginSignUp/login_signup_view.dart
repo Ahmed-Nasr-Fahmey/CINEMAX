3742607c7c6af8273f9e507768f16edf59371100
Ahmaad22 <ahmaad.ali.abdulrahman@gmail.com>
2023-10-03 01:33:01 +0300

    google auth

--- cinemax_movie_app/lib/Features/Onboarding/LoginSignUp/login_signup_view.dart
+++ cinemax_movie_app/lib/Features/Onboarding/LoginSignUp/login_signup_view.dart
@@ -1,10 +1,13 @@
 import 'package:cinemax_movie_app/Core/Shared/Customs/custom_main_button.dart';
+import 'package:cinemax_movie_app/Core/Shared/widgets/bottom_navigation_bar.dart';
 import 'package:cinemax_movie_app/Features/Onboarding/LogIn/login_view.dart';
 import 'package:cinemax_movie_app/Features/Onboarding/SignUp/sign_up_view.dart';
 import 'package:cinemax_movie_app/core/constants/colors_const.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginSignUpView extends StatelessWidget {
   const LoginSignUpView({super.key});
@@ -111,20 +114,27 @@ class LoginSignUpView extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                  width: 70,
-                  height: 70,
-                  decoration: BoxDecoration(
-                      color: const Color(0xFFF3F8FB),
-                      borderRadius: BorderRadius.circular(360)),
-                  child: Padding(
-                    padding: const EdgeInsets.all(22.0),
-                    child: Image.asset(
-                      'Assets/images/Google.png',
-                      width: 24,
-                      height: 24,
-                    ),
-                  )),
+              GestureDetector(
+                onTap: ()async{
+                   await signInWithGoogle();
+                 Navigator.pushNamed(context, CustomBottomNavigationBar.routeName);
+
+                },
+                child: Container(
+                    width: 70,
+                    height: 70,
+                    decoration: BoxDecoration(
+                        color: const Color(0xFFF3F8FB),
+                        borderRadius: BorderRadius.circular(360)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(22.0),
+                      child: Image.asset(
+                        'Assets/images/Google.png',
+                        width: 24,
+                        height: 24,
+                      ),
+                    )),
+              ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 24),
                 child: Container(
@@ -163,4 +173,20 @@ class LoginSignUpView extends StatelessWidget {
       ),
     );
   }
+}
+Future<UserCredential> signInWithGoogle() async {
+  // Trigger the authentication flow
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+  // Obtain the auth details from the request
+  final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+  // Create a new credential
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth?.accessToken,
+    idToken: googleAuth?.idToken,
+  );
+
+  // Once signed in, return the UserCredential
+  return await FirebaseAuth.instance.signInWithCredential(credential);
 }
